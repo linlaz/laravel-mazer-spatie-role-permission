6b1f2f7fb06aebf8a810b4dfc6248562f6b4128b
Lintang Lazuardi <89636741+linlaz@users.noreply.github.com>
2023-10-13 14:15:31 +0700

    Update footer.blade.php

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p>{{ date("Y") }} &copy; BEM FMIPA UNESA</p>
+            <p>{{ date("Y") }} &copy; Mazer</p>
         </div>
     </div>
 </footer>
